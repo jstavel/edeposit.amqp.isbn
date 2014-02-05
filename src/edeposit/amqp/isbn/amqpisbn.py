c941f1371b36f1ea64dde309dd206626fd31381a
Alen Dvorak <alen.dvorak@gmail.com>
2014-02-05 10:52:52 +0100

    Prepare structure

--- src/edeposit/amqp/isbn/amqpisbn.py
+++ src/edeposit/amqp/isbn/amqpisbn.py
@@ -1,1 +1,23 @@
-# ISBN module
+# -*- coding: utf-8 -*-
+#
+# AMQP ISBN module
+#
+
+from __future__ import unicode_literals
+from __future__ import print_function
+
+# AMQP framework
+from kombu import Connection
+from kombu import Exchange
+from kombu import Queue
+
+
+class IsbnMessage(object):
+    '''Base ISBN message class.'''
+    
+    pass
+
+
+class IsbnReceiver:
+    
+    pass
